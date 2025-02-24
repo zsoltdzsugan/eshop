f136063b7f2cf8c8ff69b70220b6b84953a7dfe4
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-02-24 13:01:41 +0100

    feat: admin & vendor dashboard routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\VendorController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +17,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Admin routes
+Route::middleware('auth')->group(function () {
+    Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+});
+
+// Vendor routes
+Route::middleware('auth')->group(function () {
+    Route::get('vendor/dashboard', [VendorController::class, 'dashboard'])->name('vendor.dashboard');
 });
 
 require __DIR__.'/auth.php';
